13c65c50675e2ebf2ddf6d06f762a9ad47bb51b2
fhergomez <fhergomez37@gmail.com>
2017-03-28 23:33:12 -0700

    finish first sum of All Numbers in A Range Algorithm

--- js/script.js
+++ js/script.js
@@ -1,9 +1,40 @@
 $(document).ready(function () {
 
   function sumAll(arr) {
-    return 1;
+    var list = [];
+    var lower = Math.min(arr[0], arr[1]);
+    var higher = Math.max(arr[0], arr[1]);
+
+    for (var i = lower;i <= higher;i++) {
+      list.push(i);
+    }
+    // console.log(list);
+    var sum = list.reduce(function(a,b){
+      return a + b;
+    });
+    // console.log(sum);
+    return sum;
   }
 
-  sumAll([1, 4]);
+  $('.number1').on('focus', function () {
+    $('.number1').val('');
+    $('.number2').val('Give me another Number');
+  });
+  $('.number2').on('focus', function () {
+    $('.number2').val('');
+    $('button').show();
+  })
+
+  $('.submit').on('click', function () {
+    var num1 = $('.number1').val();
+    var num2 = $('.number2').val();
+    // console.log(num1 +','+num2);
+    if (num1 < num2) {
+      $('#sumInARange').html('The sum of the range from ' + num1 + ' to ' + num2 + ' is: ' + sumAll([num1,num2]));
+    } else {
+      $('#sumInARange').html('The sum of the range from ' + num2 + ' to ' + num1 + ' is: ' + sumAll([num1,num2]));
+    }
+  });
+
 
 });
