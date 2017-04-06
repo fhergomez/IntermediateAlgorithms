4da34d423f55f8e49aae981f9b4960cb162e1ae4
fhergomez <fhergomez37@gmail.com>
2017-04-05 22:47:53 -0700

    start whatIsInAName Algorithm

--- js/whatIsInAName.js
+++ js/whatIsInAName.js
@@ -1,8 +1,13 @@
 function whatIsInAName(collection, source) {
   // What's in a name?
-  var arr = [];
-  // Only change code below this line
-
+  var arr = collection.filter(function(item) {
+    for (var i in source) {
+      if (source[i] != item[i]) {
+        return false;
+      }
+    }
+    return true;
+  });
 
   // Only change code above this line
   return arr;
